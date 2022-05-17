3c2cdcf5efebfee2a09936ac210ecd48ff1934ee
Mseymour16 <105518292+Mseymour16@users.noreply.github.com>
2022-05-17 13:06:28 -0600

    Update PlayerController.cs

--- 3D Group  Project/Assets/Scripts/PlayerController.cs
+++ 3D Group  Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 1;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(dir * speed * Time.deltaTime);
     }
 }
