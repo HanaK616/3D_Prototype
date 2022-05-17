c8fc2781315195ab134d77658a9d0f501138d545
Mseymour16 <105518292+Mseymour16@users.noreply.github.com>
2022-05-17 13:31:36 -0600

    Update PlayerCam.cs

--- 3D Group  Project/Assets/Scripts/PlayerCam.cs
+++ 3D Group  Project/Assets/Scripts/PlayerCam.cs
@@ -22,6 +22,12 @@ public class PlayerCam : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
+        yRotation += mouseX;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
