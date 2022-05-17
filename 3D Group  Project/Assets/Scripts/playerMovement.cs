0fb15991a5b11d708f080354bd1c446e52889174
Mseymour16 <105518292+Mseymour16@users.noreply.github.com>
2022-05-17 12:46:59 -0600

    Update playerMovement.cs
    fixed movement

--- 3D Group  Project/Assets/Scripts/playerMovement.cs
+++ 3D Group  Project/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+=======
+<<<<<<< HEAD
+        public float speed = 25.0F;
+        public float jumpSpeed = 8.0F;
+        public float gravity = 20.0F;
+        private Vector3 moveDirection = Vector3.zero;
+        private float turner;
+        private float looker;
+        public float sensitivity = 5;
+
+        void Update()
+        {
+            CharacterController controller = GetComponent<CharacterController>();
+            if (controller.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+
+                if (Input.GetButton("Jump"))
+                    moveDirection.y = jumpSpeed;
+
+            }
+            turner = Input.GetAxis("Mouse X") * sensitivity;
+            looker = -Input.GetAxis("Mouse Y") * sensitivity;
+            if (turner != 0)
+            {
+                transform.eulerAngles += new Vector3(0, turner, 0);
+            }
+            if (looker != 0)
+            {
+                transform.eulerAngles += new Vector3(looker, 0, 0);
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+>>>>>>> Stashed changes
     // Update is called once per frame
     void Update()
     {
         
     }
+>>>>>>> 824c650c73a47e6ef4f21e82b70b71866d6f8cbc
 }
