6b5ab9c6cfca03eab17c9c162436aeadb6fd2910
Mseymour16 <105518292+Mseymour16@users.noreply.github.com>
2022-05-19 13:20:41 -0600

    move script

---
+++ 3D Group  Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _JumpForce = 200;
+    [SerializeField] private Rigidbody _rb;
+
+    void Update()
+    {
+        var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
+        vel.y = _rb.velocity.y;
+        _rb.velocity = vel;
+
+        if (Input.GetKeyDown(KeyCode.Space)) _rb.AddForce(Vector3.up * _JumpForce);
+    }
+}
